d0b31d71b169eab6150341fdd694ef13b18efe4b
David Sherman <david.sherman@essent.nl>
2025-10-30 19:37:54 +0800

    add fetch test

--- src/operators.spec.ts
+++ src/operators.spec.ts
@@ -1,6 +1,8 @@
-import { describe, expect, test } from "vitest";
+import { afterEach, describe, expect, test } from "vitest";
+import nock from "nock";
 import { filter, map } from "./operators.js";
 import { from } from "./producers.js";
+import { pipe } from "./pipe.js";
 
 describe("operators", () => {
   test("map", async () => {
@@ -17,5 +19,38 @@ describe("operators", () => {
 
     const result = await Array.fromAsync(filtered());
     expect(result).toEqual([2, 4, 6]);
+  });
+});
+
+type Post = { id: number; title: string };
+
+describe("fetch test", () => {
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
+  test("should map fetch responses to JSON", async () => {
+    const baseUrl = "http://localhost";
+
+    const scope = nock(baseUrl)
+      .get("/posts/1")
+      .reply(200, { id: 1, title: "Post 1" })
+      .get("/posts/2")
+      .reply(200, { id: 2, title: "Post 2" });
+
+    const fetchAndParse = pipe(
+      map((id: string) => fetch(new URL(`/posts/${id}`, baseUrl))),
+      map((response) => response.json() as Promise<Post>)
+    );
+
+    const execute = fetchAndParse(from(["1", "2"]));
+    const result = await Array.fromAsync(execute());
+
+    expect(result).toEqual([
+      expect.objectContaining({ id: 1 }),
+      expect.objectContaining({ id: 2 }),
+    ]);
+
+    scope.done();
   });
 });
--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,15 @@
-export type SyncGenFn<TReturn, TArgs extends unknown[]> = (
+export type SyncGenFn<TReturn, TArgs extends unknown[] = []> = (
   ...args: TArgs
 ) => Iterable<TReturn>;
 
-export type AsyncGenFn<TReturn, TArgs extends unknown[]> = (
+export type AsyncGenFn<TReturn, TArgs extends unknown[] = []> = (
   ...args: TArgs
 ) => AsyncIterable<TReturn>;
 
-export type GenFn<TReturn, TArgs extends unknown[]> =
+export type GenFn<TReturn, TArgs extends unknown[] = []> =
   | SyncGenFn<TReturn, TArgs>
   | AsyncGenFn<TReturn, TArgs>;
 
-export type Operator<TInput, TOutput, TArgs extends unknown[]> = (
+export type Operator<TInput, TOutput, TArgs extends unknown[] = []> = (
   source: GenFn<TInput, any[]>
 ) => AsyncGenFn<TOutput, TArgs>;
