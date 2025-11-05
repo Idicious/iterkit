474cc83fff778a11af60522d177e06f27a498b27
David Sherman <david.sherman@essent.nl>
2025-11-05 12:42:48 +0800

    add merge

---
+++ src/producers.spec.ts
@@ -0,0 +1,29 @@
+import { beforeAll, describe, expect, it, vi } from "vitest";
+import { delay } from "./operators.js";
+import { merge, of } from "./producers.js";
+
+describe("producers", () => {
+  beforeAll(() => vi.useFakeTimers());
+
+  describe("merge", () => {
+    it("should merge multiple generators", async () => {
+      const gen1 = of(1, 2, 3);
+      const gen2 = of(4, 5, 6);
+      const merged = merge(gen1, gen2);
+
+      const result = await Array.fromAsync(merged());
+      expect(result).toEqual([1, 2, 3, 4, 5, 6]);
+    });
+
+    it("should merge correctly with delays", async () => {
+      const source1 = delay(10)(of("a", "b", "c"));
+      const source2 = delay(5)(of(1, 2, 3));
+      const merged = merge(source1, source2);
+
+      const resultP = Array.fromAsync(merged());
+      await vi.runAllTimersAsync();
+
+      expect(await resultP).toEqual([1, "a", 2, 3, "b", "c"]);
+    });
+  });
+});
--- src/producers.ts
+++ src/producers.ts
@@ -17,3 +17,43 @@ export function fromPromise<T>(it: Promise<T>): GenFn<T> {
 export function throwError(err: unknown): GenFn<never> {
   return fromPromise(Promise.reject(err));
 }
+
+type Active<T> = {
+  iterator: Iterator<T> | AsyncIterator<T>;
+  pending: Promise<IteratorResult<T>>;
+};
+
+export function merge<T>(...gens: GenFn<T>[]): GenFn<T> {
+  return async function* () {
+    const iterators = gens
+      .map((gen) => gen())
+      .map((it) =>
+        Symbol.asyncIterator in it
+          ? it[Symbol.asyncIterator]()
+          : it[Symbol.iterator]()
+      );
+
+    const actives = iterators.map<Active<T>>((iterator) => ({
+      iterator,
+      pending: Promise.resolve(iterator.next()),
+    }));
+
+    while (actives.length) {
+      const { index, result } = await Promise.race(
+        actives.map((active, index) =>
+          active.pending.then((result) => ({ index, result }))
+        )
+      );
+
+      const active = actives[index]!;
+
+      if (result.done) {
+        actives.splice(index, 1);
+        continue;
+      } else {
+        active.pending = Promise.resolve(active.iterator.next());
+        yield result.value;
+      }
+    }
+  };
+}
